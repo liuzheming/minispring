760eadb38c8785309adaeadce3392dfafcef437c
liuzheming <liu_0006@126.com>
2018-11-26 00:27:06 +0800

    // aop : pointcutTest add comment

--- src/main/java/mini/spring/aop/framework/AopConfig.java
+++ src/main/java/mini/spring/aop/framework/AopConfig.java
@@ -20,6 +20,12 @@ public interface AopConfig {
 
     List<Advice> getAdvice();
 
+    /**
+     * 给定一个方法,找到其对应的Advice
+     *
+     * @param method 给定的方法
+     * @return 对应的advice
+     */
     List<Advice> getAdvices(Method method/*, Class<?> targetClass */);
 
     void setTargetObject(Object obj);
