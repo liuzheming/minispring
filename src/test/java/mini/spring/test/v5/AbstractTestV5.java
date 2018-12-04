8b7538c57945ec89229be905b086cbd5df762edf
liuzheming <liuzheming@jd.com>
2018-12-04 20:21:11 +0800

    //  aop-1-3 : start

---
+++ src/test/java/mini/spring/test/v5/AbstractTestV5.java
@@ -0,0 +1,38 @@
+package mini.spring.test.v5;
+
+import mini.spring.beans.factory.BeanFactory;
+import mini.spring.beans.factory.support.BeanDefinitionRegistry;
+import mini.spring.beans.factory.support.DefaultBeanFactory;
+import mini.spring.beans.factory.xml.XMLBeanDefinitionReader;
+import mini.spring.beans.factory.xml.support.DefaultXMLBeanDefinitionReader;
+import mini.spring.core.io.support.ClassPathResource;
+import mini.spring.test.tx.TransactionMgr;
+
+import java.lang.reflect.Method;
+
+/**
+ * Description:
+ * <p>
+ * Created by lzm on  2018-12-04.
+ */
+public abstract class AbstractTestV5 {
+
+
+    BeanFactory getBeanFactory(String configFile) {
+        DefaultBeanFactory factory = new DefaultBeanFactory();
+        XMLBeanDefinitionReader reader = new DefaultXMLBeanDefinitionReader(factory);
+        reader.loadBeanDefinition(new ClassPathResource(configFile));
+        return factory;
+    }
+
+
+    Method getAdviceMethod(String methodName) throws Exception {
+        return TransactionMgr.class.getMethod(methodName);
+    }
+
+//    AspectInstanceFactory getAspectInstanceFactory(String targetBeanName) {
+//        AspectInstanceFactory aiFactory = new AspectInstanceFactory();
+
+
+//    }
+}
